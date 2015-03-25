770324a34b76177f42b205fecf9d370930348b0e
perlancar (@pc-office) <perlancar@gmail.com>
2015-03-25 12:12:08 +0700

    Add empty module to ease installation: App::pause

---
+++ lib/App/pause.pm
@@ -0,0 +1,18 @@
+package App::pause;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: An API for PAUSE
+
+=head1 DESCRIPTION
+
+This module is just an empty one to ease installation of this distribution (e.g.
+C<< cpanm -n App::pause >>). The real implementation is in
+L<WWW::PAUSE::Simple>.
+
+
+=head1 SEE ALSO
+
+L<WWW::PAUSE::Simple>
