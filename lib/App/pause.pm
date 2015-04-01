538ea38014f9aa92cef1f10457bfee2d0821852a
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2015-04-01 15:02:10 +0700

    Tweak abstract for App::pause, add link to pause cli

--- lib/App/pause.pm
+++ lib/App/pause.pm
@@ -4,15 +4,17 @@ package App::pause;
 # VERSION
 
 1;
-# ABSTRACT: An API for PAUSE
+# ABSTRACT: A CLI for PAUSE
 
 =head1 DESCRIPTION
 
 This module is just an empty one to ease installation of this distribution (e.g.
 C<< cpanm -n App::pause >>). The real implementation is in
-L<WWW::PAUSE::Simple>.
+L<WWW::PAUSE::Simple>. The CLI is L<pause>.
 
 
 =head1 SEE ALSO
 
 L<WWW::PAUSE::Simple>
+
+L<pause>
